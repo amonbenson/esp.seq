1bdbe46422c334c950d8dce524abd3fca8cc2c0f
Amon Benson <amon.k.benson@campus.tu-berlin.de>
2022-05-02 17:22:39 +0200

    using queues now

--- main/usbmidi_driver.c
+++ main/usbmidi_driver.c
@@ -6,14 +6,17 @@
 static const char *TAG = "usbmidi_driver";
 
 
+static uint32_t usbmidi_driver_add_event(usbmidi_driver_t *driver, usbmidi_event_t event) {
+    return xQueueSend(driver->events, &event, 0);
+}
+
 static void usbmidi_driver_open_dev(usbmidi_driver_t *driver) {
     assert(driver->client != NULL);
     ESP_LOGI(TAG, "opening device 0x%02x", driver->device_address);
     ESP_ERROR_CHECK(usb_host_device_open(driver->client, driver->device_address, &driver->device));
 
-    // get the device's information
-    driver->actions &= ~USBMIDI_ACTION_OPEN_DEV;
-    // TODO
+    // get the device info next
+    usbmidi_driver_add_event(driver, USBMIDI_EVENT_GET_DEV_INFO);
 }
 
 static void usbmidi_driver_close_dev(usbmidi_driver_t *driver) {
@@ -22,8 +25,6 @@ static void usbmidi_driver_close_dev(usbmidi_driver_t *driver) {
 
     driver->device = NULL;
     driver->device_address = 0;
-
-    driver->actions &= ~USBMIDI_ACTION_CLOSE_DEV;
 }
 
 static void usbmidi_midi_client_event_callback(const usb_host_client_event_msg_t *msg, void *arg) {
@@ -31,26 +32,27 @@ static void usbmidi_midi_client_event_callback(const usb_host_client_event_msg_t
 
     switch (msg->event) {
         case USB_HOST_CLIENT_EVENT_NEW_DEV:
+            if (driver->device_address != 0) break;
+
             // open new device
-            if (driver->device_address == 0) {
-                driver->device_address = msg->new_dev.address;
-                driver->actions |= USBMIDI_ACTION_OPEN_DEV;
-            }
+            driver->device_address = msg->new_dev.address;
+            usbmidi_driver_add_event(driver, USBMIDI_EVENT_OPEN_DEV);
             break;
         case USB_HOST_CLIENT_EVENT_DEV_GONE:
+            if (driver->device == NULL) break;
+
             // close device
-            if (driver->device != NULL) {
-                driver->actions |= USBMIDI_ACTION_CLOSE_DEV;
-            }
+            usbmidi_driver_add_event(driver, USBMIDI_EVENT_CLOSE_DEV);
             break;
         default:
-            abort();
+            ESP_LOGE(TAG, "unknown client event %d", msg->event);
             break;
     }
 }
 
 void usbmidi_driver_task(void *arg) {
     usbmidi_driver_t driver = { 0 };
+    driver.events = xQueueCreate(USBMIDI_EVENT_QUEUE_LEN, sizeof(usbmidi_event_t));
 
     // create the client driver
     ESP_LOGI(TAG, "registering client");
@@ -65,16 +67,25 @@ void usbmidi_driver_task(void *arg) {
     ESP_ERROR_CHECK(usb_host_client_register(&client_config, &driver.client));
 
     // handle driver events
+    usbmidi_event_t event;
     while (1) {
-        if (driver.actions == 0) {
+        // handle client events if there are no others
+        if (!xQueueReceive(driver.events, &event, 0)) {
             usb_host_client_handle_events(driver.client, portMAX_DELAY);
+            continue;    
         }
 
-        if (driver.actions & USBMIDI_ACTION_OPEN_DEV) {
-            usbmidi_driver_open_dev(&driver);
-        }
-        if (driver.actions & USBMIDI_ACTION_CLOSE_DEV) {
-            usbmidi_driver_close_dev(&driver);
+        // handle driver events
+        switch (event) {
+            case USBMIDI_EVENT_OPEN_DEV:
+                usbmidi_driver_open_dev(&driver);
+                break;
+            case USBMIDI_EVENT_CLOSE_DEV:
+                usbmidi_driver_close_dev(&driver);
+                break;
+            default:
+                ESP_LOGE(TAG, "unknown driver event %d", event);
+                break;
         }
     }
 
--- main/usbmidi_driver.h
+++ main/usbmidi_driver.h
@@ -4,15 +4,21 @@
 #include <freertos/queue.h>
 
 
-#define USBMIDI_ACTION_OPEN_DEV 0x01
-#define USBMIDI_ACTION_CLOSE_DEV 0x20
+#define USBMIDI_EVENT_QUEUE_LEN 5
 
+
+typedef enum {
+    USBMIDI_EVENT_OPEN_DEV,
+    USBMIDI_EVENT_CLOSE_DEV,
+    USBMIDI_EVENT_GET_DEV_INFO,
+    USBMIDI_EVENT_GET_DEV_DESC
+} usbmidi_event_t;
 
 typedef struct {
     usb_host_client_handle_t client;
     usb_device_handle_t device;
     uint8_t device_address;
-    uint32_t actions;
+    QueueHandle_t events;
 } usbmidi_driver_t;
 
 

cdd802da4f93bb15d21c859c81b0026b780b9b6d
Amon Benson <amon.k.benson@campus.tu-berlin.de>
2022-05-03 22:35:46 +0200

    more renames

--- components/midi/include/usb_midi.h
+++ components/midi/include/usb_midi.h
@@ -1,55 +1,44 @@
 #pragma once
 
 #include <usb/usb_host.h>
-#include <freertos/queue.h>
 #include "usb.h"
 
 
 #define USB_SUBCLASS_MIDISTREAMING 0x03
-#define USBMIDI_EVENT_QUEUE_LEN 5
 
-#define USBMIDI_PACKET_SIZE 64
+#define USB_MIDI_PACKET_SIZE 64
 
-#define USBMIDI_CIN_MISC 0x0
-#define USBMIDI_CIN_CABLE_EVENT 0x1
-#define USBMIDI_CIN_SYSCOM_2 0x2
-#define USBMIDI_CIN_SYSCOM_2_LEN 2
-#define USBMIDI_CIN_SYSCOM_3 0x3
-#define USBMIDI_CIN_SYSCOM_3_LEN 3
-#define USBMIDI_CIN_SYSEX_START 0x4
-#define USBMIDI_CIN_SYSEX_START_LEN 3
-#define USBMIDI_CIN_SYSEX_END_1_SYSCOM_1 0x5
-#define USBMIDI_CIN_SYSEX_END_1_SYSCOM_1_LEN 1
-#define USBMIDI_CIN_SYSEX_END_2 0x6
-#define USBMIDI_CIN_SYSEX_END_2_LEN 2
-#define USBMIDI_CIN_SYSEX_END_3 0x7
-#define USBMIDI_CIN_SYSEX_END_3_LEN 3
-#define USBMIDI_CIN_NOTE_OFF 0x8
-#define USBMIDI_CIN_NOTE_OFF_LEN 3
-#define USBMIDI_CIN_NOTE_ON 0x9
-#define USBMIDI_CIN_NOTE_ON_LEN 3
-#define USBMIDI_CIN_POLY_KEY_PRESSURE 0xA
-#define USBMIDI_CIN_POLY_KEY_PRESSURE_LEN 3
-#define USBMIDI_CIN_CONTROL_CHANGE 0xB
-#define USBMIDI_CIN_CONTROL_CHANGE_LEN 3
-#define USBMIDI_CIN_PROGRAM_CHANGE 0xC
-#define USBMIDI_CIN_PROGRAM_CHANGE_LEN 2
-#define USBMIDI_CIN_CHANNEL_PRESSURE 0xD
-#define USBMIDI_CIN_CHANNEL_PRESSURE_LEN 2
-#define USBMIDI_CIN_PITCH_BEND 0xE
-#define USBMIDI_CIN_PITCH_BEND_LEN 3
-#define USBMIDI_CIN_BYTE 0xF
-#define USBMIDI_CIN_BYTE_LEN 1
+#define USB_MIDI_CIN_MISC 0x0
+#define USB_MIDI_CIN_CABLE_EVENT 0x1
+#define USB_MIDI_CIN_SYSCOM_2 0x2
+#define USB_MIDI_CIN_SYSCOM_2_LEN 2
+#define USB_MIDI_CIN_SYSCOM_3 0x3
+#define USB_MIDI_CIN_SYSCOM_3_LEN 3
+#define USB_MIDI_CIN_SYSEX_START 0x4
+#define USB_MIDI_CIN_SYSEX_START_LEN 3
+#define USB_MIDI_CIN_SYSEX_END_1_SYSCOM_1 0x5
+#define USB_MIDI_CIN_SYSEX_END_1_SYSCOM_1_LEN 1
+#define USB_MIDI_CIN_SYSEX_END_2 0x6
+#define USB_MIDI_CIN_SYSEX_END_2_LEN 2
+#define USB_MIDI_CIN_SYSEX_END_3 0x7
+#define USB_MIDI_CIN_SYSEX_END_3_LEN 3
+#define USB_MIDI_CIN_NOTE_OFF 0x8
+#define USB_MIDI_CIN_NOTE_OFF_LEN 3
+#define USB_MIDI_CIN_NOTE_ON 0x9
+#define USB_MIDI_CIN_NOTE_ON_LEN 3
+#define USB_MIDI_CIN_POLY_KEY_PRESSURE 0xA
+#define USB_MIDI_CIN_POLY_KEY_PRESSURE_LEN 3
+#define USB_MIDI_CIN_CONTROL_CHANGE 0xB
+#define USB_MIDI_CIN_CONTROL_CHANGE_LEN 3
+#define USB_MIDI_CIN_PROGRAM_CHANGE 0xC
+#define USB_MIDI_CIN_PROGRAM_CHANGE_LEN 2
+#define USB_MIDI_CIN_CHANNEL_PRESSURE 0xD
+#define USB_MIDI_CIN_CHANNEL_PRESSURE_LEN 2
+#define USB_MIDI_CIN_PITCH_BEND 0xE
+#define USB_MIDI_CIN_PITCH_BEND_LEN 3
+#define USB_MIDI_CIN_BYTE 0xF
+#define USB_MIDI_CIN_BYTE_LEN 1
 
-
-typedef enum {
-    USBMIDI_EVENT_OPEN_DEV,
-    USBMIDI_EVENT_CLOSE_DEV,
-    USBMIDI_EVENT_GET_DEV_INFO,
-    USBMIDI_EVENT_GET_DEV_DESC,
-    USBMIDI_EVENT_GET_CONFIG_DESC,
-    USBMIDI_EVENT_GET_STR_DESC
-} usb_midi_event_t;
 
 typedef void (*usb_midi_device_connected_callback_t)(usb_device_desc_t *device_descriptor);
 typedef void (*usb_midi_device_disconnected_callback_t)(usb_device_desc_t *device_descriptor);
@@ -80,7 +69,6 @@ typedef struct {
 typedef struct {
     usb_driver_config_t driver_config;
     usb_midi_config_t config;
-    QueueHandle_t events;
 
     usb_host_client_handle_t client;
     usb_device_handle_t device;
--- components/midi/src/usb_midi.c
+++ components/midi/src/usb_midi.c
@@ -15,11 +15,7 @@
 static const char *TAG = "usb_midi";
 
 
-static uint32_t usbmidi_driver_add_event(usb_midi_t *usb_midi, usb_midi_event_t event) {
-    return xQueueSend(usb_midi->events, &event, 0);
-}
-
-static void usbmidi_driver_handle_data_in(usb_midi_t *usb_midi, uint8_t *data, int len) {
+static void usb_midi_handle_data_in(usb_midi_t *usb_midi, uint8_t *data, int len) {
     usb_midi_callbacks_t *callbacks = &usb_midi->config.callbacks;
 
     // validate mininum length
@@ -34,66 +30,66 @@ static void usbmidi_driver_handle_data_in(usb_midi_t *usb_midi, uint8_t *data, i
     int msg_len = len - 1;
 
     switch (cin) {
-        case USBMIDI_CIN_MISC:
+        case USB_MIDI_CIN_MISC:
             ESP_LOGE(TAG, "misc messages unimplemented");
             break;
-        case USBMIDI_CIN_CABLE_EVENT:
+        case USB_MIDI_CIN_CABLE_EVENT:
             ESP_LOGE(TAG, "cable event messages unimplemented");
             break;
-        case USBMIDI_CIN_SYSCOM_2:
-            if (msg_len < USBMIDI_CIN_SYSCOM_2_LEN) return;
+        case USB_MIDI_CIN_SYSCOM_2:
+            if (msg_len < USB_MIDI_CIN_SYSCOM_2_LEN) return;
             ESP_LOGE(TAG, "syscom 2 messages unimplemented");
             break;
-        case USBMIDI_CIN_SYSCOM_3:
-            if (msg_len < USBMIDI_CIN_SYSCOM_3_LEN) return;
+        case USB_MIDI_CIN_SYSCOM_3:
+            if (msg_len < USB_MIDI_CIN_SYSCOM_3_LEN) return;
             ESP_LOGE(TAG, "syscom 3 messages unimplemented");
             break;
-        case USBMIDI_CIN_SYSEX_START:
-            if (msg_len < USBMIDI_CIN_SYSEX_START_LEN) return;
+        case USB_MIDI_CIN_SYSEX_START:
+            if (msg_len < USB_MIDI_CIN_SYSEX_START_LEN) return;
             ESP_LOGE(TAG, "sysex start messages unimplemented");
             break;
-        case USBMIDI_CIN_SYSEX_END_1_SYSCOM_1:
-            if (msg_len < USBMIDI_CIN_SYSEX_END_1_SYSCOM_1_LEN) return;
+        case USB_MIDI_CIN_SYSEX_END_1_SYSCOM_1:
+            if (msg_len < USB_MIDI_CIN_SYSEX_END_1_SYSCOM_1_LEN) return;
             ESP_LOGE(TAG, "sysex end 1 syscom 1 messages unimplemented");
             break;
-        case USBMIDI_CIN_SYSEX_END_2:
-            if (msg_len < USBMIDI_CIN_SYSEX_END_2_LEN) return;
+        case USB_MIDI_CIN_SYSEX_END_2:
+            if (msg_len < USB_MIDI_CIN_SYSEX_END_2_LEN) return;
             ESP_LOGE(TAG, "sysex end 2 messages unimplemented");
             break;
-        case USBMIDI_CIN_SYSEX_END_3:
-            if (msg_len < USBMIDI_CIN_SYSEX_END_3_LEN) return;
+        case USB_MIDI_CIN_SYSEX_END_3:
+            if (msg_len < USB_MIDI_CIN_SYSEX_END_3_LEN) return;
             ESP_LOGE(TAG, "sysex end 3 messages unimplemented");
             break;
-        case USBMIDI_CIN_NOTE_OFF:
-            if (msg_len < USBMIDI_CIN_NOTE_OFF_LEN) return;
+        case USB_MIDI_CIN_NOTE_OFF:
+            if (msg_len < USB_MIDI_CIN_NOTE_OFF_LEN) return;
             USB_MIDI_INVOKE_CALLBACK(callbacks, note_off, msg[0] & 0x0f, msg[1], msg[2]);
             break;
-        case USBMIDI_CIN_NOTE_ON:
-            if (msg_len < USBMIDI_CIN_NOTE_ON_LEN) return;
+        case USB_MIDI_CIN_NOTE_ON:
+            if (msg_len < USB_MIDI_CIN_NOTE_ON_LEN) return;
             USB_MIDI_INVOKE_CALLBACK(callbacks, note_on, msg[0] & 0x0f, msg[1], msg[2]);
             break;
-        case USBMIDI_CIN_POLY_KEY_PRESSURE:
-            if (msg_len < USBMIDI_CIN_POLY_KEY_PRESSURE_LEN) return;
+        case USB_MIDI_CIN_POLY_KEY_PRESSURE:
+            if (msg_len < USB_MIDI_CIN_POLY_KEY_PRESSURE_LEN) return;
             USB_MIDI_INVOKE_CALLBACK(callbacks, poly_key_pressure, msg[0] & 0x0f, msg[1], msg[2]);
             break;
-        case USBMIDI_CIN_CONTROL_CHANGE:
-            if (msg_len < USBMIDI_CIN_CONTROL_CHANGE_LEN) return;
+        case USB_MIDI_CIN_CONTROL_CHANGE:
+            if (msg_len < USB_MIDI_CIN_CONTROL_CHANGE_LEN) return;
             USB_MIDI_INVOKE_CALLBACK(callbacks, control_change, msg[0] & 0x0f, msg[1], msg[2]);
             break;
-        case USBMIDI_CIN_PROGRAM_CHANGE:
-            if (msg_len < USBMIDI_CIN_PROGRAM_CHANGE_LEN) return;
+        case USB_MIDI_CIN_PROGRAM_CHANGE:
+            if (msg_len < USB_MIDI_CIN_PROGRAM_CHANGE_LEN) return;
             USB_MIDI_INVOKE_CALLBACK(callbacks, program_change, msg[0] & 0x0f, msg[1]);
             break;
-        case USBMIDI_CIN_CHANNEL_PRESSURE:
-            if (msg_len < USBMIDI_CIN_CHANNEL_PRESSURE_LEN) return;
+        case USB_MIDI_CIN_CHANNEL_PRESSURE:
+            if (msg_len < USB_MIDI_CIN_CHANNEL_PRESSURE_LEN) return;
             USB_MIDI_INVOKE_CALLBACK(callbacks, channel_pressure, msg[0] & 0x0f, msg[1]);
             break;
-        case USBMIDI_CIN_PITCH_BEND:
-            if (msg_len < USBMIDI_CIN_PITCH_BEND_LEN) return;
+        case USB_MIDI_CIN_PITCH_BEND:
+            if (msg_len < USB_MIDI_CIN_PITCH_BEND_LEN) return;
             USB_MIDI_INVOKE_CALLBACK(callbacks, pitch_bend, msg[0] & 0x0f, (msg[1] | msg[2] << 7) - 8192);
             break;
-        case USBMIDI_CIN_BYTE:
-            if (msg_len < USBMIDI_CIN_BYTE_LEN) return;
+        case USB_MIDI_CIN_BYTE:
+            if (msg_len < USB_MIDI_CIN_BYTE_LEN) return;
             ESP_LOGE(TAG, "single byte messages unimplemented");
             break;
         default:
@@ -102,11 +98,11 @@ static void usbmidi_driver_handle_data_in(usb_midi_t *usb_midi, uint8_t *data, i
     }
 }
 
-static void usbmidi_driver_data_in_callback(usb_transfer_t *transfer) {
+static void usb_midi_data_in_callback(usb_transfer_t *transfer) {
     usb_midi_t *usb_midi = (usb_midi_t *) transfer->context;
 
     // handle the incoming data
-    usbmidi_driver_handle_data_in(usb_midi, transfer->data_buffer, transfer->actual_num_bytes);
+    usb_midi_handle_data_in(usb_midi, transfer->data_buffer, transfer->actual_num_bytes);
 
     // continue polling if the device hasn't been closed yet
     // usb_host_transfer_submit might return an invalid state error,
@@ -116,41 +112,14 @@ static void usbmidi_driver_data_in_callback(usb_transfer_t *transfer) {
     }
 }
 
-static void usbmidi_driver_data_out_callback(usb_transfer_t *transfer) {
+static void usb_midi_data_out_callback(usb_transfer_t *transfer) {
     usb_midi_t *usb_midi = (usb_midi_t *) transfer->context;
     ESP_LOGI(TAG, "data out callback");
 }
 
-static void usbmidi_driver_open_dev(usb_midi_t *usb_midi) {
-    assert(usb_midi->device_address != 0);
-    ESP_LOGI(TAG, "opening device at 0x%02x", usb_midi->device_address);
-    ESP_ERROR_CHECK(usb_host_device_open(usb_midi->client, usb_midi->device_address, &usb_midi->device));
-
-    // get the device info next
-    usbmidi_driver_add_event(usb_midi, USBMIDI_EVENT_GET_DEV_INFO);
-}
-
-static void usbmidi_driver_get_dev_info(usb_midi_t *usb_midi) {
+static esp_err_t usb_midi_open_device(usb_midi_t *usb_midi, uint8_t address) {
+    esp_err_t err;
     usb_device_info_t info;
-
-    assert(usb_midi->device != NULL);
-    ESP_LOGI(TAG, "getting device information");
-    ESP_ERROR_CHECK(usb_host_device_info(usb_midi->device, &info));
-
-    // get the device descriptor next
-    usbmidi_driver_add_event(usb_midi, USBMIDI_EVENT_GET_DEV_DESC);
-}
-
-static void usbmidi_driver_get_dev_desc(usb_midi_t *usb_midi) {
-    assert(usb_midi->device != NULL);
-    ESP_LOGI(TAG, "getting device descriptor");
-    ESP_ERROR_CHECK(usb_host_get_device_descriptor(usb_midi->device, &usb_midi->device_descriptor));
-
-    // get the configuration descriptor next
-    usbmidi_driver_add_event(usb_midi, USBMIDI_EVENT_GET_CONFIG_DESC);
-}
-
-static void usbmidi_driver_get_config_desc(usb_midi_t *usb_midi) {
     const usb_config_desc_t *descriptor;
     const usb_standard_desc_t *d;
     int offset = 0;
@@ -159,11 +128,29 @@ static void usbmidi_driver_get_config_desc(usb_midi_t *usb_midi) {
     const usb_ep_desc_t *data_in = NULL;
     const usb_ep_desc_t *data_out = NULL;
 
+    // open the device
+    usb_midi->device_address = address;
+    ESP_LOGI(TAG, "opening device at 0x%02x", usb_midi->device_address);
+    err = usb_host_device_open(usb_midi->client, usb_midi->device_address, &usb_midi->device);
+    if (err != ESP_OK) return err;
+
+    // get the device info
+    ESP_LOGI(TAG, "getting device information");
+    err = usb_host_device_info(usb_midi->device, &info);
+    if (err != ESP_OK) return err;
+
+    // get the device descriptor
+    ESP_LOGI(TAG, "getting device descriptor");
+    err = usb_host_get_device_descriptor(usb_midi->device, &usb_midi->device_descriptor);
+    if (err != ESP_OK) return err;
+
+    // get the configuration descriptor
     assert(usb_midi->device != NULL);
     ESP_LOGI(TAG, "getting configuration descriptor");
-    ESP_ERROR_CHECK(usb_host_get_active_config_descriptor(usb_midi->device, &descriptor));
+    usb_host_get_active_config_descriptor(usb_midi->device, &descriptor);
+    if (err != ESP_OK) return err;
 
-    // scan all descriptors
+    // scan the configuration descriptor for interfaces and endpoints
     for (d = (usb_standard_desc_t *) descriptor; d != NULL; d = usb_parse_next_descriptor(d, descriptor->wTotalLength, &offset)) {
         switch (d->bDescriptorType) {
             case USB_W_VALUE_DT_INTERFACE:;
@@ -198,67 +185,55 @@ static void usbmidi_driver_get_config_desc(usb_midi_t *usb_midi) {
         }
     }
 
-    // check if we've got all the descriptors
+    // check if we've got all the necessary descriptors
     if (!interface || !data_in || !data_out) {
         ESP_LOGE(TAG, "not a valid MIDI device");
-        return;
+        return ESP_FAIL;
     }
     
     // claim the interface
     ESP_LOGI(TAG, "claiming interface");
-    ESP_ERROR_CHECK(usb_host_interface_claim(usb_midi->client,
+    err = usb_host_interface_claim(usb_midi->client,
         usb_midi->device,
         interface->bInterfaceNumber,
-        interface->bAlternateSetting));
+        interface->bAlternateSetting);
+    if (err != ESP_OK) return err;
 
     // store the interface and create transfers for each endpoint
     usb_midi->interface = interface;
 
-    size_t data_in_size = MIN(USB_EP_DESC_GET_MPS(data_in), USBMIDI_PACKET_SIZE);
-    ESP_ERROR_CHECK(usb_host_transfer_alloc(data_in_size, 0, &usb_midi->data_in));
+    size_t data_in_size = MIN(USB_EP_DESC_GET_MPS(data_in), USB_MIDI_PACKET_SIZE);
+    err = usb_host_transfer_alloc(data_in_size, 0, &usb_midi->data_in);
+    if (err != ESP_OK) return err;
+
     usb_midi->data_in->device_handle = usb_midi->device;
     usb_midi->data_in->bEndpointAddress = data_in->bEndpointAddress;
-    usb_midi->data_in->callback = usbmidi_driver_data_in_callback;
+    usb_midi->data_in->callback = usb_midi_data_in_callback;
     usb_midi->data_in->context = (void *) usb_midi;
     usb_midi->data_in->num_bytes = data_in_size;
     
-    size_t data_out_size = MIN(USB_EP_DESC_GET_MPS(data_out), USBMIDI_PACKET_SIZE);
-    ESP_ERROR_CHECK(usb_host_transfer_alloc(data_out_size, 0, &usb_midi->data_out));
+    size_t data_out_size = MIN(USB_EP_DESC_GET_MPS(data_out), USB_MIDI_PACKET_SIZE);
+    err = usb_host_transfer_alloc(data_out_size, 0, &usb_midi->data_out);
+    if (err != ESP_OK) return err;
+
     usb_midi->data_out->device_handle = usb_midi->device;
     usb_midi->data_out->bEndpointAddress = data_out->bEndpointAddress;
-    usb_midi->data_out->callback = usbmidi_driver_data_out_callback;
+    usb_midi->data_out->callback = usb_midi_data_out_callback;
     usb_midi->data_out->context = (void *) usb_midi;
 
     // invoke the connected callback
     USB_MIDI_INVOKE_CALLBACK(&usb_midi->config.callbacks, connected, usb_midi->device_descriptor);
 
     // start input polling
-    ESP_ERROR_CHECK(usb_host_transfer_submit(usb_midi->data_in));
+    err = usb_host_transfer_submit(usb_midi->data_in);
+    if (err != ESP_OK) return err;
 
     ESP_LOGI(TAG, "midi device initialized (data in size: %d, data out size: %d)", data_in_size, data_out_size);
+
+    return ESP_OK;
 }
 
-static void usbmidi_driver_get_str_desc(usb_midi_t *usb_midi) {
-    usb_device_info_t info;
-
-    assert(usb_midi->device != NULL);
-    ESP_LOGI(TAG, "getting string descriptors");
-    ESP_ERROR_CHECK(usb_host_device_info(usb_midi->device, &info));
-
-    if (info.str_desc_manufacturer) {
-        usb_print_string_descriptor(info.str_desc_manufacturer);
-    }
-
-    if (info.str_desc_product) {
-        usb_print_string_descriptor(info.str_desc_product);
-    }
-
-    if (info.str_desc_serial_num) {
-        usb_print_string_descriptor(info.str_desc_serial_num);
-    }
-}
-
-static void usbmidi_driver_close_dev(usb_midi_t *usb_midi) {
+static void usb_midi_close_device(usb_midi_t *usb_midi) {
     assert(usb_midi->device != NULL);
 
     // invoke the disconnected callback if the interface was actually claimed
@@ -294,7 +269,7 @@ static void usbmidi_driver_close_dev(usb_midi_t *usb_midi) {
     usb_midi->device_address = 0;
 }
 
-static void usbmidi_midi_client_event_callback(const usb_host_client_event_msg_t *msg, void *arg) {
+static void usb_midi_client_event_callback(const usb_host_client_event_msg_t *msg, void *arg) {
     usb_midi_t *usb_midi = (usb_midi_t *) arg;
 
     switch (msg->event) {
@@ -302,14 +277,13 @@ static void usbmidi_midi_client_event_callback(const usb_host_client_event_msg_t
             if (usb_midi->device_address != 0) break;
 
             // open new device
-            usb_midi->device_address = msg->new_dev.address;
-            usbmidi_driver_add_event(usb_midi, USBMIDI_EVENT_OPEN_DEV);
+            usb_midi_open_device(usb_midi, msg->new_dev.address);
             break;
         case USB_HOST_CLIENT_EVENT_DEV_GONE:
             if (usb_midi->device == NULL) break;
 
             // close device
-            usbmidi_driver_add_event(usb_midi, USBMIDI_EVENT_CLOSE_DEV);
+            usb_midi_close_device(usb_midi);
             break;
         default:
             ESP_LOGE(TAG, "unknown client event %d", msg->event);
@@ -320,54 +294,21 @@ static void usbmidi_midi_client_event_callback(const usb_host_client_event_msg_t
 void usb_midi_driver_task(void *arg) {
     usb_midi_t *usb_midi = (usb_midi_t *) arg;
 
-    // create the event queue
-    usb_midi->events = xQueueCreate(USBMIDI_EVENT_QUEUE_LEN, sizeof(usb_midi_event_t));
-
     // create the client usb_midi
     ESP_LOGI(TAG, "registering client");
     usb_host_client_config_t client_config = {
         .is_synchronous = false,
         .max_num_event_msg = 5,
         .async = {
-            .client_event_callback = usbmidi_midi_client_event_callback,
+            .client_event_callback = usb_midi_client_event_callback,
             .callback_arg = (void *) usb_midi
         }
     };
     ESP_ERROR_CHECK(usb_host_client_register(&client_config, &usb_midi->client));
 
-    // handle usb_midi events
-    usb_midi_event_t event;
+    // main event loop
     while (1) {
-        // handle client events if there are no others
-        if (!xQueueReceive(usb_midi->events, &event, 0)) {
-            usb_host_client_handle_events(usb_midi->client, portMAX_DELAY);
-            continue;    
-        }
-
-        // handle usb_midi events
-        switch (event) {
-            case USBMIDI_EVENT_OPEN_DEV:
-                usbmidi_driver_open_dev(usb_midi);
-                break;
-            case USBMIDI_EVENT_GET_DEV_INFO:
-                usbmidi_driver_get_dev_info(usb_midi);
-                break;
-            case USBMIDI_EVENT_GET_DEV_DESC:
-                usbmidi_driver_get_dev_desc(usb_midi);
-                break;
-            case USBMIDI_EVENT_GET_CONFIG_DESC:
-                usbmidi_driver_get_config_desc(usb_midi);
-                break;
-            case USBMIDI_EVENT_GET_STR_DESC:
-                usbmidi_driver_get_str_desc(usb_midi);
-                break;
-            case USBMIDI_EVENT_CLOSE_DEV:
-                usbmidi_driver_close_dev(usb_midi);
-                break;
-            default:
-                ESP_LOGE(TAG, "unknown usb_midi event %d", event);
-                break;
-        }
+        usb_host_client_handle_events(usb_midi->client, portMAX_DELAY);
     }
 
     // cleanup
